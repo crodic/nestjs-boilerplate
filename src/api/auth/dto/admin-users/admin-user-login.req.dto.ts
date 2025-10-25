6c6e27097014a180cd50188b2b9bc465f985b7bd
Crodic <alice01422@gmail.com>
2025-10-25 13:37:06 +0700

    remove auth module

--- src/api/auth/dto/admin-users/admin-user-login.req.dto.ts
+++
@@ -1,9 +0,0 @@
-import { EmailField, PasswordField } from '@/decorators/field.decorators';
-
-export class AdminUserLoginReqDto {
-  @EmailField({ toLowerCase: false })
-  email!: string;
-
-  @PasswordField()
-  password!: string;
-}
--- src/api/auth/dto/users/login.req.dto.ts
+++
@@ -1,9 +0,0 @@
-import { EmailField, PasswordField } from '@/decorators/field.decorators';
-
-export class LoginReqDto {
-  @EmailField({ toLowerCase: false })
-  email!: string;
-
-  @PasswordField()
-  password!: string;
-}
