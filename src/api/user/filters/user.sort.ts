ab524803c8faac618517674024c51b63dd282357
Crodic <alice01422@gmail.com>
2025-12-28 10:07:43 +0700

    update

--- src/api/user/filters/user.sort.ts
+++
@@ -1,3 +0,0 @@
-import { UserIdSort } from './id.sort';
-
-export const USER_SORTS = [new UserIdSort()];
--- src/api/user/filters/users.filter.ts
+++
@@ -1,8 +0,0 @@
-import { QueryFilter } from '@/common/interfaces/filter.interface';
-import { SearchFilter } from './name.filter';
-import { TypeFilter } from './type.filter';
-
-export const USER_FILTERS: QueryFilter[] = [
-  new SearchFilter(),
-  new TypeFilter(),
-];
